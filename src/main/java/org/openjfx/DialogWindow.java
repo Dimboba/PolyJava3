a443dd2e8a6924ef70ef28387c25e20d7849963e
Dimboba <lazarkovdmitry@yandex.ru>
2022-12-20 01:51:51 +0300

    Created GUI (kinda)

---
+++ src/main/java/org/openjfx/DialogWindow.java
@@ -0,0 +1,8 @@
+package org.openjfx;
+
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+
+public class DialogWindow extends Dialog<ButtonType> {
+
+}
--- src/main/java/org/openjfx/MainApp.java
+++ src/main/java/org/openjfx/MainApp.java
@@ -1,7 +1,25 @@
 package org.openjfx;
 
-public class MainApp {
-    public static void main(String[] args){
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.stage.*;
 
+import java.util.Optional;
+
+public class MainApp extends Application {
+
+
+    @Override public void start(Stage mainStage) throws Exception{
+        Group root = new Group();
+        Scene scene = new Scene(root, 500, 500);
+        mainStage.setScene(scene);
+        mainStage.setTitle("Testing");
+        DialogWindow dialogWindow = new DialogWindow();
+        mainStage.show();
+    }
+    public static void main(String[] args){
+        launch(args);
     }
 }
